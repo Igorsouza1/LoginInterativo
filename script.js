79eb03108fa68aa9c13651d02db7a312a1dc3c5c
Igor Souza <igor1_souza@hotmail.com>
2021-04-26 22:50:07 -0400

    AdiçãoJS

---
+++ script.js
@@ -0,0 +1,53 @@
+let validadorJs = {
+    handleSubmit:(event)=>{
+        event.preventDefault();
+        let send = true;
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++){
+            let input = inputs[i];
+            let check = validadorJs.checkInput(input);
+            if(check !== true){
+                send = false;
+                //exibir o erro
+                validadorJs.showError(input, check);
+            }
+        }
+
+        if(send){
+            form.submit();
+        }
+    },
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null){
+            rules = rules.split('|');
+            for(let k in rules){
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value == ''){
+                         return 'Campo Obrigatorio'
+                        }
+                    break;
+                    case 'min':
+
+                    break;
+                }
+            }
+        }
+        return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = 'red';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input);
+    }
+    
+}
+let form = document.querySelector('.validadorJs');
+form.addEventListener('submit', validadorJs.handleSubmit);
+
